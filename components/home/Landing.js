687f35b089c475497ddb49eaabf834814f8a0d2f
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-07-27 20:05:26 -0400

    add images to public & first pass at landing images

--- components/home/Landing.js
+++ components/home/Landing.js
@@ -1,3 +1,12 @@
+import styles from '../../styles/Landing.module.scss';
+
 export default function Landing() {
-    return <p>Landing</p>
+    return <>
+        <div className={styles['background-img-wrapper']}>
+            <img src='/images/sky.png' alt='background' />
+        </div>
+        {/* TODO: probably wrap this in a div along w/ the signpost */}
+        {/* TODO: make sure my approach for these images works on mobile */}
+        <img className={styles['clouds-img']} src="/images/clouds.png" alt="clouds" />
+    </>;
 }
