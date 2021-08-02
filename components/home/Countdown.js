720c3afa4e57796eb347474dad54c08c8be2238c
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-08-01 22:38:41 -0400

    countdown tweaks

--- components/home/Countdown.js
+++ components/home/Countdown.js
@@ -6,12 +6,12 @@ export default function MyCountdown() {
     return <div className="flex-container">
         <div className={styles['countdown-container']}>
             <h2>October 3rd to 4th, 2021</h2>
-            <p>Schedule, challenges, and prizes releasd in:</p>
+            <p>Schedule, challenges, and prizes revealed in:</p>
             <Countdown
-                date={Date.parse("2021-10-03T00:00:00+0000")}
-                renderer={({ hours, minutes, seconds }) => (
+                date={Date.parse("2021-09-28T00:00:00+0000")}
+                renderer={({ days, hours, minutes, seconds }) => (
                     <span className={styles['countdown']}>
-                        {hours}&nbsp;:&nbsp;{minutes}&nbsp;:&nbsp;{seconds}
+                        {days}&nbsp;:&nbsp;{hours}&nbsp;:&nbsp;{minutes}&nbsp;:&nbsp;{seconds}
                     </span>
                 )}
             />
