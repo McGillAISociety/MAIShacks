fe946a8af240386b20b5a88e59320b014aa42268
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-08-02 13:19:43 -0400

    fix connecty boi positioning bugs

--- components/home/Info.js
+++ components/home/Info.js
@@ -1,25 +1,22 @@
 import styles from '../../styles/Info.module.scss';
-import { useMediaQuery } from 'react-responsive';
 
 export default function Info() {
 
-    // TODO: figure out how to make this work with next...?
-    const showConnecty = useMediaQuery({ minWidth: 900 });
-
     return <>
         <div className="flex-container">
-            <div className={'text-container'}
-                style={{paddingRight: showConnecty ? '200px': 0}}>
-                    <h1>Canada&apos;s Largest AI Hackathon.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis venenatis turpis maximus cursus. Etiam dapibus risus ut neque mollis accumsan. Suspendisse imperdiet vitae nulla ac consectetur.
-                    </p>
-                    <p>
-                    Donec dictum ut nulla nec ultricies. Morbi non enim molestie felis viverra efficitur vel ac libero. Suspendisse potenti. Phasellus eget faucibus dui. Nam luctus eleifend rhoncus. Ut eleifend quam orci, ac varius ligula posuere ac. Donec id ornare enim. Aliquam vel metus arcu. Vestibulum ante ipsum primis in faucibus.
-                    </p>
-                </div>
-            {showConnecty && <img src="/images/connecty_boi.png" alt="Connecty Boi"
-                className={styles['connecty-boi']}
-            />}
+            <div>
+                <div className={'text-container'}>
+                        <h1>Canada&apos;s Largest AI Hackathon.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque venenatis venenatis turpis maximus cursus. Etiam dapibus risus ut neque mollis accumsan. Suspendisse imperdiet vitae nulla ac consectetur.
+                        </p>
+                        <p>
+                        Donec dictum ut nulla nec ultricies. Morbi non enim molestie felis viverra efficitur vel ac libero. Suspendisse potenti. Phasellus eget faucibus dui. Nam luctus eleifend rhoncus. Ut eleifend quam orci, ac varius ligula posuere ac. Donec id ornare enim. Aliquam vel metus arcu. Vestibulum ante ipsum primis in faucibus.
+                        </p>
+                    </div>
+                <img src="/images/connecty_boi.png" alt="Connecty Boi"
+                    className={`${styles['connecty-boi']} mobile-hide`}
+                />
+            </div>
         </div>
         <div className="flex-container">
             <div className={`${styles['bubble']} ${styles['bubble--left']}`}>
