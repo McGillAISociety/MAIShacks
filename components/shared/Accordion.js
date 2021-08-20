be604c27462df2df9561c7345200cc5cf6741d35
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-08-20 16:31:57 -0400

    use JPG for backgroup

--- components/shared/Accordion.js
+++ components/shared/Accordion.js
@@ -3,7 +3,6 @@ import styles from "../../styles/Accordion.module.scss";
 import { FaPlus, FaMinus } from 'react-icons/fa'
 
 // TODO: I find the unopened state styling a bit flat; change?
-// TODO: animate the icon/label on hover/click?
 // TODO: animate the content expanding?
 
 export default function Accordion({label, children}) {
