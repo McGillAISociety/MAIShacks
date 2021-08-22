895ce2c289e096cf4dcd0787001d9b89be98cb23
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2021-08-22 19:29:22 -0400

    wording changes

--- components/home/Info.js
+++ components/home/Info.js
@@ -9,7 +9,7 @@ export default function Info() {
                         <h1>Canada&apos;s Largest AI Hackathon Goes Hybrid.</h1>
                         <p>Innovation lies at the heart of MAIS Hacks, with past winners building solutions for topics ranging between music composition, plant pathology, and security patrols.
                         </p>
-                        <p>This year, we&apos;re thrilled to offer a hybrid format, with in-person components offered (with strong health and safety protocols) at <a href="https://mila.quebec/en/" target="_blank" rel="noreferrer">Mila - the Quebec AI Institute</a>!</p>
+                        <p>This year, contingent upon the COVID-19 situation in Montréal and with strong health protocols in place, we&apos;re thrilled to offer a hybrid format with in-person components at <a href="https://mila.quebec/en/" target="_blank" rel="noreferrer">Mila - the Quebec AI Institute</a>!</p>
                         <p>
                         Regardless of whether you’re an AI novice or veteran, everyone is welcome to join. Just bring a desire to learn about how AI can be used to overcome societal problems and boost competitiveness – and we’ll provide the mentors, computing credits, and swag.
                         </p>
@@ -22,8 +22,8 @@ export default function Info() {
         <div className="flex-container">
             <div className={`card-border ${styles['bubble']} ${styles['bubble--left']}`}>
                 <h2>Social Networking and Team Making</h2>
-                <p>Don&apos;t worry if you apply alone, there will be many resources dedicated to networking with your peers and team matching!</p>
-                <p>Whether you&apos;re participating online or in-person, MAIS Hacks provides many social activities where you can have fun and make new connections.</p>
+                <p>Don&apos;t worry if you apply alone, there will be ample opportunities to connecting with your peers and matching yourself with the perfect team!</p>
+                <p>Whether you&apos;re participating online or in-person, MAIS Hacks provides various resources, social activities and networking opportunities to help you make lasting connections.</p>
             </div>
             <div className={`card-border ${styles['bubble']} ${styles['bubble--center']}`}>
                 <h2>Workshops and Activities</h2>
