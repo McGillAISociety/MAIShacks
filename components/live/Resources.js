1d6409fa9505cb3450cb53a96ae398ffdbf5bc02
Signe Hoel <signe.hoel@gmail.com>
2023-09-23 11:52:52 -0400

    edit live schedule

--- components/live/Resources.js
+++ components/live/Resources.js
@@ -1,38 +1,56 @@
-import {
-    FaDiscord as Discord,
-    FaDev as Devpost
-} from 'react-icons/fa';
+import { FaDiscord as Discord, FaDev as Devpost } from "react-icons/fa";
 
 import Button from "../shared/Button";
-import styles from '../../styles/Resources.module.scss';
+import styles from "../../styles/Resources.module.scss";
 
 const iconProps = { size: 30 };
 
 const resources = [
-    { icon: <Discord {...iconProps} />, text: "Discord", link: "https://discord.gg/eAkXjqkpet" },
-    { icon: <Devpost {...iconProps} />, text: "Devpost", link: "https://maishacks-2022.devpost.com/" }
-]
+  {
+    icon: <Discord {...iconProps} />,
+    text: "Discord",
+    link: "https://discord.gg/96fjuebv",
+  },
+  {
+    icon: <Devpost {...iconProps} />,
+    text: "Devpost",
+    link: "https://maishacks2023.devpost.com/",
+  },
+];
 
 export default function Resources() {
-    return <div className='top-margin'>
-        <small className='text-center'>
-            Join the Discord and Devpost <i>only if you have been accepted to MAIS Hacks!</i> You will be kicked otherwise.
-        </small>
-        <br></br>
-        <small className='text-center'>
-            Have some feedback? Submit it anonymously <a href="https://docs.google.com/forms/d/e/1FAIpQLSefYnGQjK0PG6opR2-oZ6sDXYp3crOE_Y6TkTKCqNWGoXNrog/viewform" target="_blank" rel="noreferrer">here.</a>
-        </small>
-        <div className="flex-container">
-            {resources.map(((resource, index) =>
-                <div key={index} className={styles['button-wrapper']}>
-                    <Button onClick={() => window.open(resource.link)}
-                            disabled={!resource.link}>
-                        <div className="flex">
-                            {resource.icon} &nbsp; {resource.text}
-                        </div>
-                    </Button>
-                </div>
-            ))}
-        </div>
+  return (
+    <div className="top-margin">
+      <small className="text-center">
+        Join the Discord and Devpost{" "}
+        <i>only if you have been accepted to MAIS Hacks!</i> You will be kicked
+        otherwise.
+      </small>
+      <br></br>
+      <small className="text-center">
+        Have some feedback? Submit it anonymously{" "}
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSefYnGQjK0PG6opR2-oZ6sDXYp3crOE_Y6TkTKCqNWGoXNrog/viewform"
+          target="_blank"
+          rel="noreferrer"
+        >
+          here.
+        </a>
+      </small>
+      <div className="flex-container">
+        {resources.map((resource, index) => (
+          <div key={index} className={styles["button-wrapper"]}>
+            <Button
+              onClick={() => window.open(resource.link)}
+              disabled={!resource.link}
+            >
+              <div className="flex">
+                {resource.icon} &nbsp; {resource.text}
+              </div>
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
+  );
 }
--- pages/live.js
+++ pages/live.js
@@ -1,18 +1,24 @@
 import Resources from "../components/live/Resources";
-import Schedule from '../components/live/Schedule';
-import Sponsors from '../components/shared/Sponsors';
-import MetaTags from '../components/shared/MetaTags';
-import LiveLanding from '../components/live/LiveLanding';
+import Schedule from "../components/live/Schedule";
+import Sponsors from "../components/shared/Sponsors";
+import MetaTags from "../components/shared/MetaTags";
+import LiveLanding from "../components/live/LiveLanding";
 
 export default function Live() {
-    return <>
-        <MetaTags title="LIVE | MAIS Hacks 2022" description="Canada's largest AI hackathon"
-            imagePath="/meta/banner.png" route="/live" />
-        <LiveLanding />
-        <Resources />
-        <Schedule />
-        <div className="top-margin">
-            <Sponsors justImages />
-        </div>
-    </>;
+  return (
+    <>
+      <MetaTags
+        title="LIVE | MAIS Hacks 2023"
+        description="Canada's largest AI hackathon"
+        imagePath="/meta/banner.png"
+        route="/live"
+      />
+      <LiveLanding />
+      <Resources />
+      <Schedule />
+      <div className="top-margin">
+        <Sponsors justImages />
+      </div>
+    </>
+  );
 }
